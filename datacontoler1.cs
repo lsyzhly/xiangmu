44946cf95475428a015efdf22f72b801297fc156
cjwddtc <leeshengyuan@live.com>
2015-07-06 11:23:29 +0800

    test

---
+++ datacontoler1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Data.SQLite;
+
+class datacontrol{
+	DbConnection connection;
+	DbCommand cmd;
+	public datacontrol(string path){
+		bool flag;
+		if(File. Exists(path)){
+			
+		}
+	}
+}
