40ae33c86d33ef8c8ac7a32d332f60339b02415d
cjwddtc <leeshengyuan@live.com>
2015-07-06 12:20:22 +0800

    能运行

--- datacontoler1.cs
+++ datacontoler1.cs
@@ -7,7 +7,7 @@ using System.Data.SQLite;
 class datacontrol{
 	DbConnection connection;
 	DbCommand cmd;
-	public datacontrol(string path){
+	public datacontrol(String path){
 		bool flag;
 		if(File. Exists(path)){
 			flag=false;
@@ -19,7 +19,16 @@ class datacontrol{
 		connection=con;
 		cmd=new SQLiteCommand(con);
 		if(flag){
+			cmd.CommandText="create table driver(driverid varchar PRIMARY KEY,personid varchar not null,sex bool not null,name varchar not null,birthday char(8) not null,password varchar not null)";
+			cmd.ExecuteNonQuery();
 			//TODO create the table
 		}
+	}
+	void insert(String driverid,String personid,bool sex,String name,String birthday,String password){
+		cmd.CommandText=String.Format("insert into driver values ('{0}','{1}',{2},'{3}','{4}','{5}')",driverid,personid,sex,name,birthday,password);
+		cmd.ExecuteNonQuery();
+	}
+	static void Main(string[] argvs){
+		
 	}
 }
