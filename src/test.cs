926d82ec96f8cbe84cbf5a47a7af9c6831c81ac8
cjwddtc <leeshengyuan@live.com>
2015-07-06 19:46:51 +0800

    修改makefile文件的bug以及增加lsy.cs的少数实现

--- src/test.cs
+++ src/test.cs
@@ -1,9 +1,5 @@
 class test{
 	public static void Main(string[] argvs){
 		datacontrol a=new datacontrol("asd.data");
-		a.insert("adm","cjwddtc");
-		a.updateAdminstratorPassword("adm","1234");
-		a.insert("123","456",true,"789","asd","fgh");
-		//a.updateDriverName("123",)
 	}
 }
