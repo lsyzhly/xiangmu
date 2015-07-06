4770114093713a25d4adbe8ab6488d6ad880ae21
cjwddtc <leeshengyuan@live.com>
2015-07-06 18:38:53 +0800

    add hanhan to master

--- src/datacontoler.cs
+++ src/datacontoler.cs
@@ -26,6 +26,9 @@ class datacontrol{
 			cmd.ExecuteNonQuery();
 			cmd.CommandText="insert into adminstrator values(\'adminstrator\',\'1234\')";
 			cmd.ExecuteNonQuery();
+			
+			cmd.CommandText="create table car(carid varchar primary key,caryear int,avaliable bool,insurancen bool,yearcheck bool)";
+			cmd.ExecuteNonQuery();
 			//TODO create the table
 		}
 	}
@@ -36,6 +39,13 @@ class datacontrol{
 	public void insert(String id,String password){
 		cmd.CommandText=String.Format("insert into adminstrator values ('{0}','{1}')",id,password);
 		cmd.ExecuteNonQuery();
+	}
+	public void insert(String theCarid,int theCaryear,bool theAvaliable,bool theInsurance,bool theYearCheck)
+	{   
+	    String cmdTemp;
+		cmdTemp=String.Format("insert into car values ('{0}',{1},{2},{3},{4})",theCarid,theCaryear,theAvaliable?1:0,theInsurance?1:0,theYearCheck?1:0);
+		cmd.CommandText = cmdTemp;
+        cmd.ExecuteNonQuery();
 	}
 	public void updateDriverName(String id,String str){
 		cmd.CommandText=String.Format("update driver set name='{0}' where driverid='{1}'",str,id);
@@ -48,5 +58,27 @@ class datacontrol{
 	public void updateAdminstratorPassword(String id,String password){
 		cmd.CommandText=String.Format("update adminstrator set password='{0}' where id='{1}'",password,id);
 		cmd.ExecuteNonQuery();
+	}
+	
+	public void updateAvaliable(String theCarid,bool theAvaliable)
+	{
+		String cmdTemp;
+		cmdTemp=String.Format("update car set avaliable={0} where carid='{1}'",theAvaliable?1:0,theCarid);
+		cmd.CommandText = cmdTemp;
+        cmd.ExecuteNonQuery();
+	}
+	public void updateInsurance(String theCarid,bool theInsurance)
+	{
+		String cmdTemp;
+		cmdTemp=String.Format("update car set insurancen={0} where carid='{1}'",theInsurance?1:0,theCarid);
+		cmd.CommandText = cmdTemp;
+        cmd.ExecuteNonQuery();
+	}
+	public void updateYearCheck(String theCarid,bool theYearCheck)
+	{
+		String cmdTemp;
+		cmdTemp=String.Format("update car set yearcheck={0} where carid='{1}'",theYearCheck?1:0,theCarid);
+		cmd.CommandText = cmdTemp;
+        cmd.ExecuteNonQuery();
 	}
 }
