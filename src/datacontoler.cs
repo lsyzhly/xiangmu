47ca0dea92eac0549f042ecf37c5bae83f310465
cjwddtc <leeshengyuan@live.com>
2015-07-07 10:02:45 +0800

    add avaliableCar

--- src/datacontoler.cs
+++ src/datacontoler.cs
@@ -133,4 +133,15 @@ class datacontrol{
 	public bool isCarAvaliable(Car carid,int startdate,int end){
 		return false;
 	}
+	public void avaliableCar()
+	{
+		string cmdTemp;
+		string nowDate;
+		int tempDate;
+		DateTime dt = DateTime.Now;
+		nowDate=dt.ToString("yyyyMMdd");
+		tempDate=int.Parse(nowDate);
+		cmdTemp=String.Format("select * from car where carid not in(select carid from contrcst where isvalid=1 and {0}<endDate and isagree=1) and avaliable=1",tempDate);
+		//TODO ... 
+	}
 }
