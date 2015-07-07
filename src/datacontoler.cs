0fb5c55ded928ea8fc6bd783beb0d47cdfe61119
MonkeyDli <752566207@qq.com>
2015-07-07 11:33:13 +0800

    getcontract

--- src/datacontoler.cs
+++ src/datacontoler.cs
@@ -144,4 +144,23 @@ class datacontrol{
 		cmdTemp=String.Format("select * from car where carid not in(select carid from contract where isvalid=1 and {0}<endDate and isagree=1) and avaliable=1 and (caryear+8)-{1}>=0",tempDate,end);
 		//TODO ... 
 	}
+	public Contract getContract(string personid)
+	{
+		DateTime now = DateTime.Now;
+		String  tmp = now.ToString("yyyyMMdd");
+		int date = Convert.ToInt32(tmp);
+		cmd.CommandText = String.Format("select * from contract where startdate <={0} and enddate>={1} and isvalid = 1",date,date);
+		DbDataReader data = cmd.ExecuteReader();
+		if(data.Read())
+		{
+			String carid = data.GetString(1);
+			String driverid = data.GetString(2);
+			int startdate = data.GetInt32(3);
+			int enddate = data.GetInt32(4);
+			bool isagree = data.GetBoolean(5);
+			bool isvalid = data.GetBoolean(6);
+			return new Contract(carid,driverid,startdate,enddate,isagree,isvalid);
+		}
+		return null;
+	}
 }
