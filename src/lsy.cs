5c520f23e0a97944741da039bdecbb9cab96c7b6
cjwddtc <leeshengyuan@live.com>
2015-07-08 12:19:17 +0800

    add

--- src/lsy.cs
+++ src/lsy.cs
@@ -1,10 +1,10 @@
 using System;
  public class Driver{
-	String driverid;
-	String personid;
-	bool sex;
-	String name;
-	String birthday;
+	public String driverid;
+    public String personid;
+    public bool sex;
+    public String name;
+    public String birthday;
 	public String password;
 	public Driver(String did,String pid,bool se,String na,String bith,String pass){
 		driverid=did;
